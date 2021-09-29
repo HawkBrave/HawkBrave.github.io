fbdb2b1e56653d964bddfd07edf1a3858e0e8b37
HawkBrave <doganyigityilmazz@gmail.com>
2021-09-29 14:03:16 +0100

    init

--- script.js
+++ script.js
@@ -14,29 +14,32 @@ let epoch = 3;
 const grayscaleSet = false;
 
 
-function mandelbrot(c) {
+function mandelbrot(c, z={n: 0, re: 0, im: 0}) {
   let alpha = 1.0;
-  let z = {re: 0, im: 0};
-  for (let i = 0; i < epoch; i+=2) {
+  for (let i = z.n; i < epoch; i+=2) {
     let p = {
       re: Math.pow(z.re, 2) - Math.pow(z.im, 2),
       im: 2 * z.re * z.im
     }
     z.re = p.re + c.re;
     z.im = p.im + c.im;
+    z.n++;
 
     if (Math.pow(z.re, 2) + Math.pow(z.im, 2) > 4) {
       alpha = parseFloat(i) / epoch;
       break;
     }
   }
-  return alpha;
+  return alpha; 
 }
 
 function content() {
   const content = document.querySelector('#content');
   content.style.opacity = '1';
 }
+
+let mbState = Array.from(Array(width), () => new Array(height));
+mbState.forEach(r => r.forEach(z => z = {n: 0, re: 0, im: 0}));
 
 function render() {
   if (epoch >= 60) {
@@ -50,9 +53,10 @@ function render() {
       let c = {
         re: realSet.start + (i / width) * (realSet.end - realSet.start),
         im: imaginarySet.start + (j / height) * (imaginarySet.end - imaginarySet.start)
-      }
+      };
 
       let alpha = mandelbrot(c);
+
       if (grayscaleSet) {
         let scale = (Math.abs(alpha - 1) * 255).toPrecision(3);
         ctx.fillStyle = `rgb(${scale}, ${scale}, ${scale})`;
@@ -67,7 +71,7 @@ function render() {
 }
 
 function main() {
-  setTimeout(() => {canvas.style.opacity = 1;}, 300);
+  setTimeout(() => {canvas.style.opacity = 1;}, 200);
   render();
 }
 
