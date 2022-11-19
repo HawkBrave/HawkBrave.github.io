17e1ea49986682368a867df2c96c8afcebb741f7
yvesyil <doganyigityilmazz@gmail.com>
2022-11-19 23:21:59 +0000

    addition and improvement of content

--- src/display.js
+++ src/display.js
@@ -3,18 +3,38 @@ import Utils from "../utils/utils.js";
 export default class Display {
   constructor() {
     this.container = document.querySelector('#container');
-    this.context = {busy: false};
+    this.context = {busy: {unfade: false, move: false}};
   }
 
+  /**
+   * 
+   * @param {HTMLElement} node 
+   */
   load(node) {
-    this.buffer = node;
+    this.title = node.children.namedItem('section-title');
+    this.node = node;
   }
 
-  show(fading) {
-    if (fading && !this.context.busy) {
-      Utils.unfade(this.container, this.context);
+  async show(fading) {
+    if (this.title) {
+      if (fading && !this.context.busy['unfade']) {
+        Utils.unfade(this.title, this.context);
+      }
+      this.container.replaceChild(this.title, this.container.children[0]);
+      await Utils.sleepUntilContextIsFree(this.context, 'unfade');
+      await Utils.sleep(100);
+      Utils.move(this.title, 'up', this.getHeight()/2, this.getHeight()/2 + 200, this.context);
+      await Utils.sleepUntilContextIsFree(this.context, 'move');
+    } 
+    
+    this.container.replaceChild(this.node, this.container.children[0]);
+    
+    if (fading && !this.context.busy['unfade']) {
+      Utils.unfade(this.node.children.namedItem('section-body'), this.context);
     }
-    this.container.replaceChild(this.buffer, this.container.children[0]);
+    if (this.title) {
+      this.container.children[0].insertBefore(this.title, this.container.children[0].children[0]);
+    }
   }
 
   getHeight = () => window.innerHeight;
